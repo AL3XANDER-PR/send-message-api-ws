f1b39429e87cb3be1e16c7dbb0161ff973396925
Alexander Ponte Reynaldo <alexanderponte7@gmail.com>
2024-07-17 16:15:53 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys"
+import { createBot, createProvider, createFlow, MemoryDB } from '@bot-whatsapp/bot'
+
+
+const main = async () => {
+    const provider = createProvider(BaileysProvider)
+
+    provider.initHttpServer(3002)
+    provider.http?.server.post('/send',handleCtx(async (bot,req,res)=>{
+        console.log(req.body)
+        const phone = req.body.phone
+        const message = req.body.message
+        await bot.sendMessage(phone,message,{})
+        res.end()
+    }))
+
+    await createBot({
+        flow: createFlow([]),   
+        database: new MemoryDB(),
+        provider
+    })
+
+
+}
+
+main()
